efc6a1a9a86cc5c59fabb9363211bdbf5f31e279
rodneybw <meuter.arno@gmail.com>
2017-01-23 17:15:09 +0100

    Basic main and resource building test

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func GetIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	file, _ := ResourceFile("index.html")
+	fmt.Fprint(w, "<!-- test -->")
+	fmt.Fprint(w, string(file))
+}
+
+func GetStylesheet(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/css; charset=utf-8")
+	file, _ := ResourceFile("styles.css")
+	fmt.Fprint(w, string(file))
+}
+
+func GetScript(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/javascript; charset=utf-8")
+	file, _ := ResourceFile("scripts.js")
+	fmt.Fprint(w, string(file))
+}
+
+func main() {
+
+	router := httprouter.New()
+	router.GET("/", GetIndex)
+	router.GET("/styles.css", GetStylesheet)
+	router.GET("/scripts.js", GetScript)
+
+	log.Fatal(http.ListenAndServe(":54321", router))
+}
